640d7bff6725d4d37b73b260c16b452a9a156126
nachista <igmartin4@yahoo.com>
2016-06-29 23:00:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Esther? Just say HELLO to get started.')
+            return bot.say('You've found Ignacio's internet personal assistant. Say HELLO if this isn't too awkward for you' )
                 .then(() => 'speak');
         }
     },
